adf1d212165695bcb29acabb7cdf266beda81cbc
David McCann <david.a.mccann@gmail.com>
2011-08-31 11:47:31 +0300

    initial packages

---
+++ project/urls.py
@@ -0,0 +1,42 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.contrib import admin
+from rapidsms_httprouter.urls import urlpatterns as router_urls
+from ureport.urls import urlpatterns as ureport_urls
+from rapidsms_xforms.urls import urlpatterns as xform_urls
+from healthmodels.urls import urlpatterns as healthmodels_urls
+from contact.urls import urlpatterns as contact_urls
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    
+    # RapidSMS core URLs
+    (r'^account/', include('rapidsms.urls.login_logout')),
+    url(r'^$', 'cvs.views.basic.index', name='rapidsms-dashboard'),    
+    url('^accounts/login', 'rapidsms.views.login'),
+    url('^accounts/logout', 'rapidsms.views.logout'),
+
+    # RapidSMS contrib app URLs
+    (r'^ajax/', include('rapidsms.contrib.ajax.urls')),
+    (r'^export/', include('rapidsms.contrib.export.urls')),
+    (r'^httptester/', include('rapidsms.contrib.httptester.urls')),
+    (r'^locations/', include('rapidsms.contrib.locations.urls')),
+    (r'^messagelog/', include('rapidsms.contrib.messagelog.urls')),
+    (r'^messaging/', include('rapidsms.contrib.messaging.urls')),
+    (r'^registration/', include('auth.urls')),
+    (r'^scheduler/', include('rapidsms.contrib.scheduler.urls')),
+    (r'^polls/', include('poll.urls')),
+) + router_urls + ureport_urls + xform_urls + healthmodels_urls + contact_urls 
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        # helper URLs file that automatically serves the 'static' folder in
+        # INSTALLED_APPS via the Django static media server (NOT for use in
+        # production)
+        (r'^', include('rapidsms.urls.static_media')),
+    )
+
+from rapidsms_httprouter.router import get_router
+get_router()
+
