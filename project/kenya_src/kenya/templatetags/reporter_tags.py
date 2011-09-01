aad82e6061adad20f7c0ff79847928e155707e56
David McCann <david.a.mccann@gmail.com>
2011-09-01 12:18:07 +0300

    template tag fixes

--- project/kenya_src/kenya/templatetags/reporter_tags.py
+++ project/kenya_src/kenya/templatetags/reporter_tags.py
@@ -2,9 +2,16 @@ from django import template
 
 def get_district(location):
     try:
-        return location.get_ancestors().get(kind__name='district').name
+        return location.get_ancestors().get(type__name='district').name
+    except:
+        return None
+
+def latest(obj):
+    try:
+        return XFormSubmission.objects.filter(connection__in=obj.connection_set.all()).latest('created').created
     except:
         return None
 
 register = template.Library()
 register.filter('get_district', get_district)
+register.filter('latest', latest)
